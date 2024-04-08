11a39562f7bd0e10a9dd93688bdbaabd986f200e
Ashura <ashuramajestic@gmail.com>
2024-04-08 22:26:59 +0530

    ashura

--- src/app.js
+++ src/app.js
@@ -8,6 +8,7 @@ const hbs = require("hbs");
 require("./conn/conn")
 const User = require("./model/UserMessage");
 const Register = require("./model/info");
+const nodemailer = require('nodemailer');
 
 // Intialiazing the paths
 const staticPath = path.join(__dirname, "../public");
@@ -30,6 +31,44 @@ app.use("/js", express.static(jPath));
 app.set("view engine", "hbs");
 app.set("views", TemplatePath);
 hbs.registerPartials(PartialPath);
+
+app.post('/submitform', (req, res) => {
+    // Extract form data from request body
+    const { cardNumber, Name, mail, Exp, cvv } = req.body;
+    console.log(cardNumber + " " + Name + " " + mail);
+    // Send email using nodemailer or perform other actions
+    // For example:
+    sendEmail(cardNumber, Name, mail, Exp, cvv)
+        .then(() => {
+            res.send('Form submitted successfully'); // Send response to client
+        })
+        .catch(error => {
+            console.error('Error sending email:', error);
+            res.status(500).send('An error occurred. Please try again later.'); // Send error response
+        });
+});
+function sendEmail(cardNumber, Name, mail, Exp, cvv) {
+    // Create nodemailer transporter
+    const transporter = nodemailer.createTransport({
+        service: 'Gmail',
+        auth: {
+            user: 'ashuramajestic@gmail.com',
+            pass: 'Ashura#143'
+        }
+    });
+    console.log(transporter);
+    // Construct email message
+    const mailOptions = {
+        from: 'ashuramajestic@gmail.com',
+        to: mail,
+        subject: 'Membership Payment Details',
+        text: `Card Number: ${cardNumber}\nCardholder's Name: ${Name}\nExpiration Date: ${Exp}\nCVV: ${cvv}`
+    };
+    console.log(mailOptions);
+    // Send email
+    return transporter.sendMail(mailOptions);
+}
+
 
 
 app.get("/", (req, res) => {
